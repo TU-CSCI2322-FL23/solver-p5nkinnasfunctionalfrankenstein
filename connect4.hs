06e72d678f787a9500ea46ee18fb0108a0f3781e
wwelden <wwelden@trinity.edu>
2023-10-30 13:43:56 -0500

    check win

--- connect4.hs
+++ connect4.hs
@@ -1,8 +1,9 @@
 import Data.ByteString (find)
+import Control.Concurrent.STM (check)
 
 
 --data Board =  -- board size
-data Player = Red | Black
+data Player = Red | Black deriving (Show, Eq)
 type Game =  [[Int]]
 
 
@@ -56,6 +57,31 @@ switchPlayer :: Player -> Player
 switchPlayer Red = Black
 switchPlayer Black = Red
 
+-- still working on checkWinRow/Col/Diag
+
+checkWinRow :: Game -> Player -> Bool
+checkWinRow gm ply = any (== True) (map (checkWinRow' ply) gm)
+    where checkWinRow' ply row = any (== True) (map (checkWinRow'' ply row) [1..length row])
+          checkWinRow'' ply row n = getElemAtDex row n == playerToInt ply && getElemAtDex row (n+1) == playerToInt ply && getElemAtDex row (n+2) == playerToInt ply && getElemAtDex row (n+3) == playerToInt ply
+          playerToInt Red = 1
+          playerToInt Black = 2
+checkWinCol :: Game -> Player -> Bool
+checkWinCol gm ply = any (== True) (map (checkWinCol' ply) gm)
+    where checkWinCol' ply col = any (== True) (map (checkWinCol'' ply col) [1..length col])
+          checkWinCol'' ply col n = getElemAtDex col n == playerToInt ply && getElemAtDex col (n+1) == playerToInt ply && getElemAtDex col (n+2) == playerToInt ply && getElemAtDex col (n+3) == playerToInt ply
+          playerToInt Red = 1
+          playerToInt Black = 2
+checkWinDiag :: Game -> Player -> Bool
+checkWinDiag gm ply = checkWinDiag' gm ply || checkWinDiag' (reverse gm) ply
+    where checkWinDiag' gm ply = any (== True) (map (checkWinDiag'' ply) gm)
+          checkWinDiag'' ply row = any (== True) (map (checkWinDiag''' ply row) [1..length row])
+          checkWinDiag''' ply row n = getElemAtDex row n == playerToInt ply && getElemAtDex (tail row) (n+1) == playerToInt ply && getElemAtDex (tail (tail row)) (n+2) == playerToInt ply && getElemAtDex (tail (tail (tail row))) (n+3) == playerToInt ply
+          playerToInt Red = 1
+          playerToInt Black = 2
+
+checkWin :: Game -> Player -> Bool
+checkWin gm ply = checkWinRow gm ply || checkWinCol gm ply || checkWinDiag gm ply
+
 playGame :: Game -> Player -> IO ()
 playGame gm ply = do
     displayGame gm
@@ -63,7 +89,7 @@ playGame gm ply = do
     col <- getLine
     let newGm = makeMove (read col) gm ply
     displayGame newGm
-    playGame newGm (switchPlayer ply)
+    if checkWin newGm ply then putStrLn (show ply ++ " wins!") else playGame newGm (switchPlayer ply)
 
 main :: IO ()
 main = do
